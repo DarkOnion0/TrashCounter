9fa05626da43b287936c8b02039942ad56b74eb0
DarkOnion0 <Darkgenius1@protonmail.com>
2021-07-04 18:35:07 +0200

    update nav component

--- app/src/components/Nav.jsx
+++ app/src/components/Nav.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 
 import "./../css/Nav.css"
 import "./../css/Main.css"
@@ -7,15 +7,36 @@ import "./../css/Main.css"
 class Nav extends React.Component {
   render() {
     return (
-      <ul className="flex">
-        <Link to="/">
-          <li>Home</li>
-        </Link>
+      <div id="NavBar" className="grid-pancake">
+        <NavLink
+          to="/"
+          className="link-normal"
+          exact
+          activeClassName="link-active"
+        >
+          Home
+        </NavLink>
 
-        <Link to="/Dev">
-          <li>Dev</li>
-        </Link>
-      </ul>
+        <NavLink
+          to="/dev"
+          className="link-normal"
+          exact
+          activeClassName="link-active"
+        >
+          Dev
+        </NavLink>
+
+        <spacer></spacer>
+
+        <NavLink
+          to="/settings"
+          className="link-normal"
+          exact
+          activeClassName="link-active"
+        >
+          Settings
+        </NavLink>
+      </div>
     )
   }
 }
