9b132a2cdd49d92841d16f9948c52aa1dfb07052
DarkOnion0 <Darkgenius1@protonmail.com>
2021-07-08 20:09:46 +0200

    change key name and default value

--- app/src/components/DateSelector.jsx
+++ app/src/components/DateSelector.jsx
@@ -4,8 +4,10 @@ import "./../css/Main.css"
 import "./../css/DateSelector.css"
 
 function DateSelector(props) {
+  sessionStorage.setItem("trash-date", null)
+
   function handleChange(event) {
-    sessionStorage.setItem("date-trash", event.target.value)
+    sessionStorage.setItem("trash-date", event.target.value)
   }
 
   return <input type="date" onChange={handleChange} />
