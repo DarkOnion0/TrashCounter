6d670b8c079362bf324db8ef4b1c54b3248cb7dc
DarkOnion0 <Darkgenius1@protonmail.com>
2021-07-12 10:57:09 +0200

    add the Stats page

---
+++ app/src/pages/Stats.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+import "./../css/Stats.css"
+import "./../css/Main.css"
+
+function Stats() {
+  return <div id="stats" className="page-frame"></div>
+}
+
+export default Stats
