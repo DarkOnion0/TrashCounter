1986488e2a8eecf8c8b5205f15278e34c7dff8c0
DarkOnion0 <Darkgenius1@protonmail.com>
2021-10-22 12:16:19 +0200

    add a github login button

--- app/src/pages/Settings.jsx
+++ app/src/pages/Settings.jsx
@@ -1,17 +1,30 @@
 import React from "react"
+import { useEffect, useState } from "react"
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import {
+  faCloud,
   faDownload,
   faTrashAlt,
   faUpload,
+  faSave,
+  faTimesCircle,
 } from "@fortawesome/free-solid-svg-icons"
 
 import "./../css/Settings.css"
 import "./../css/Main.css"
 import TrashList from "../components/TrashList"
+import Popup from "../components/Popup"
+
+const axios = require("axios").default
 
 function Settings(props) {
+  useEffect(() => {
+    dynamicGithubConnection()
+  }, [])
+
+  const [content, setContent] = useState(<p>Anythings has been set</p>)
+
   function importData() {
     const importData = document.createElement("input")
     importData.setAttribute("type", "file")
@@ -104,14 +117,114 @@ function Settings(props) {
     window.location.reload(true)
   }
 
+  function dynamicGithubConnection() {
+    console.log(JSON.parse(localStorage.getItem("sync")))
+
+    if (JSON.parse(localStorage.getItem("sync"))) {
+      document.getElementById("syncGithub").innerHTML = "Disconnect from GitHub"
+    } else {
+      document.getElementById("syncGithub").innerHTML = "Connect to GitHub"
+    }
+  }
+
+  function syncEvent(event) {
+    event.preventDefault()
+
+    if (!JSON.parse(localStorage.getItem("sync"))) {
+      document.getElementById("syncPopup").style.display = "block"
+
+      setContent(
+        <div id="syncFrame">
+          <div id="githubSecretContainer">
+            <input
+              id="githubSecretInput"
+              type="password"
+              placeholder="Paste your GitHub GIST secret there"
+            />
+          </div>
+
+          <div id="buttonContainer" className="buttonContainerMul">
+            <button
+              className="button buttonIcon"
+              onClick={(event) => {
+                event.preventDefault()
+                var token = document.getElementById("githubSecretInput").value
+                document.getElementById("githubSecretInput").value = null
+
+                if (token.length === 0) {
+                  alert("Please don't set an empty token !")
+                } else {
+                  const res = axios
+                    .get("https://api.github.com/user", {
+                      headers: {
+                        Authorization: `token ${token}`,
+                      },
+                    })
+                    .then(function (response) {
+                      console.log(response)
+
+                      localStorage.setItem("githubToken", JSON.stringify(token))
+                      localStorage.setItem("sync", JSON.stringify(true))
+                      dynamicGithubConnection()
+                      document.getElementById("syncPopup").style.display =
+                        "none"
+                    })
+                }
+              }}
+            >
+              <FontAwesomeIcon icon={faSave} />
+              <p>Save</p>
+            </button>
+            <button
+              className="button buttonIcon"
+              onClick={(event) => {
+                event.preventDefault()
+                document.getElementById("syncPopup").style.display = "none"
+              }}
+            >
+              <FontAwesomeIcon icon={faTimesCircle} />
+              <p>Discard</p>
+            </button>
+          </div>
+        </div>
+      )
+    } else {
+      localStorage.setItem("sync", JSON.stringify(false))
+      localStorage.removeItem("githubToken")
+      dynamicGithubConnection()
+    }
+  }
+
   return (
     <div className="pageWrapper">
       <div className="pageScrollContainer">
         <div id="settings" className="page-frame">
           <div id="content-display">
-            <div id="data-container" className="display-container">
-              <h2>Data</h2>
+            <div id="trash-container" className="display-container">
+              <h2>Trash</h2>
+              <TrashList type="settings" />
+            </div>
 
+            <div id="data-container" className="display-container">
+              <h2>Synchronization</h2>
+
+              <div>
+                <div id="data-button-container" className="flex-row">
+                  <div>
+                    <button className="buttonIcon button" onClick={syncEvent}>
+                      <FontAwesomeIcon icon={faCloud} />
+                      <p id="syncGithub">Connect to GitHub</p>
+                    </button>
+                  </div>
+                </div>
+                <Popup
+                  id="syncPopup"
+                  title="Connect to GitHub"
+                  content={content}
+                />
+              </div>
+
+              <h2>Data</h2>
               <div id="data-button-container" className="flex-row">
                 <div>
                   <button className="buttonIcon button" onClick={importData}>
@@ -130,11 +243,6 @@ function Settings(props) {
               </div>
             </div>
 
-            <div id="trash-container" className="display-container">
-              <h2>Trash</h2>
-              <TrashList type="settings" />
-            </div>
-
             <div id="news-container" className="display-container">
               <div>
                 <h2>News</h2>
@@ -145,7 +253,12 @@ function Settings(props) {
                 <h2>App information</h2>
                 <ul id="appInfo">
                   <li>
-                    App version: {JSON.parse(localStorage.getItem("version"))}
+                    <strong>App version:</strong>{" "}
+                    {localStorage.getItem("version")}
+                  </li>
+                  <li>
+                    <strong>App synchronization:</strong>{" "}
+                    {JSON.stringify(localStorage.getItem("sync"))}
                   </li>
                 </ul>
               </div>
