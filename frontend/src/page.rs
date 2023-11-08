118f27a8dd57d7be75023f77378ee6a306b029cf
DarkOnion0 <darkonion0@clapot.org>
2023-11-08 18:24:57 +0100

    feat(frontend/calendar): made the add button responsive

--- frontend/src/page.rs
+++ frontend/src/page.rs
@@ -3,7 +3,7 @@ use leptos::*;
 #[component]
 pub fn Calendar() -> impl IntoView {
     view! {
-        <button class="absolute right-[2rem] bottom-[calc(2rem+4rem)] btn btn-square hover:btn-circle">
+        <button class="absolute right-[2rem] bottom-[calc(2rem+4rem)] lg:bottom-[calc(2rem)] btn lg-max:btn-square lg-max:hover:btn-circle">
             <svg
                 xmlns="http://www.w3.org/2000/svg"
                 class="h-6 w-6 rotate-45"
@@ -18,6 +18,7 @@ pub fn Calendar() -> impl IntoView {
                     d="M6 18L18 6M6 6l12 12"
                 ></path>
             </svg>
+            <p class="inline lg-max:!hidden">Add a trash</p>
         </button>
     }
 }
