8decab16b5390c8fa2a111a458610cf9984413a5
DarkOnion0 <darkonion0@clapot.org>
2023-11-08 18:23:56 +0100

    build(frontend/tailwind): add screens

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,4 +3,18 @@ module.exports = {
     files: ["*.html", "./src/**/*.rs"],
   },
   plugins: [require("daisyui")],
+  theme: {
+    screens: {
+      'sm': '640px',
+
+      'md': '768px',
+
+      'lg': '1024px',
+      'lg-max': {'max': '1023px'},
+
+      'xl': '1280px',
+
+      '2xl': '1536px',
+    }
+  },
 };
